77e3fc7d63c9ca9f3518635eb51b3d6aaec11be8
MI396547 <mithilesh.pradhan@wipro.com>
2018-06-22 19:07:33 +0530

    initial commit

---
+++ books.js
@@ -0,0 +1,18 @@
+var sbm = $('input[type=submit]');
+$(document).ready(function(){
+    
+    sbm.on('click',function(){
+         var title = $('#title').val();
+         var genre = $('#genre').val();
+         var content = $('#content').val();
+
+         
+     
+        })
+   
+
+})
+
+
+
+//e8zs7QBRLZ-PtH2eRqPIF1uEajuKrJBq
